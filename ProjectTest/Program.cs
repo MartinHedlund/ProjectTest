fcb48e65f31f39c9ba299a1d6b53253ce377b775
MartinHedlund <seva447@gmail.com>
2023-07-03 11:51:45 +0300

    add programm test

--- ProjectTest/Program.cs
+++ ProjectTest/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello World!");
+﻿using GeometryLibrary;
+
+IShape circle = new Circle (radius: 5);
+double circleArea = circle.CalculateArea();
+Console.WriteLine($"Площадь круга: {circleArea}");
+
+IShape triangle = new Triangle(sideA: 3, sideB: 4, sideC: 5); ;
+double triangleArea = triangle.CalculateArea();
+Console.WriteLine($"Площадь треугольника: {triangleArea}");
